d64210bdd321b040f322dd70f8075288ffd4423a
Hans Chaudry <62669434+HansChaudry@users.noreply.github.com>
2024-09-18 07:24:01 -0400

    passive buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,35 @@ Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 
 
 #define i2c_Address 0x3c // initialize with the I2C addr 0x3C Typically eBay OLED's
 
-const int buttonPin = 2; 
+const int buttonPin = 2;
+const int buzzerPin = 8;
 int buttonState = 0;
 
 void setup() {
   Serial.begin(9600); // Start serial communication for debugging
   display.begin(i2c_Address);
-  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setCursor(0, 0);
-  display.println("Hello Duino on Mac");
-  display.display();
   
   pinMode(buttonPin, INPUT_PULLUP); // Use internal pull-up resistor
+  pinMode(buzzerPin, OUTPUT); // Use internal pull-up resistor
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
 
-  // Print button state to Serial Monitor for debugging
-  Serial.print("Button state: ");
-  Serial.println(buttonState);
-
   display.clearDisplay(); // Clear display
   display.setCursor(0, 0); // Set cursor to top-left corner
 
   if (buttonState == HIGH) { // Button pressed (assuming pull-up configuration)
-    display.print("Button pressed");
+    display.print("Btn pressed");
+    tone(buzzerPin, 550); // A4
+    delay(200);
   } else {
     display.print("Press btn");
+    noTone(buzzerPin);
+    
   }
   
   display.display(); // Update the display
